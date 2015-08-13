5c23c7c69aa8283f7c4c949a2760adc5c6252f72
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-13 02:21:17 -0700

    made tooltips work properly

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    Opentip.styles.leagueItems = {
+        extends: "alert",
+        stem: true,
+        borderColor: "rgb(0, 204, 153)",
+        borderWidth: 1,
+        background: "rbga(0, 0, 0, 0.2)",
+        borderRadius: 5,
+        offset: [ 0, 0 ],
+        tipJoint: "bottom left",
+        stem: false
+    };
+    Opentip.defaultStyle = "leagueItems";
+
     $("#item-set-add-block-button").click(function() {
         $("#item-set-blocks").append('<li><div class="collapsible-header grey-text text-darken-2" contentEditable=true>New Item Block</div><div class="collapsible-body grey lighten-3 grey-text text-darken-2"><p>Lorem ipsum dolor sit amet.</p></div></li>');
         $(".collapsible").collapsible({
@@ -10,7 +23,8 @@ $(document).ready(function() {
         dataJSON = JSON.parse(data);
         for (var itemId in dataJSON) {
             if (dataJSON.hasOwnProperty(itemId)) {
-                $("#all-items").append('<img id="' + itemId + '" class="item" src="/images/items/' + itemId + '.png" alt="' + dataJSON[itemId]['name'] + '"/>')
+                $("#all-items").append('<img id="' + itemId + '" class="item" src="/images/items/' + itemId + '.png" alt="' + dataJSON[itemId]['name'] + '"/>');
+                new Opentip("#" + itemId, "<img src='/images/gold.png'>" + dataJSON[itemId]['gold']['total'] + "<br><br>" + dataJSON[itemId]['description'], dataJSON[itemId]['name'])
                 dataJSON[itemId]['tags'].forEach(function(tag) {
                     $("#" + itemId).addClass(tag);
                 });
