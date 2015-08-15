8910f859567916d42d5ee417e6fd81651542fafc
Jason Yang <jasonyang96@ucla.edu>
2015-08-15 13:19:49 -0500

    correct var name

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -49,15 +49,15 @@ $(document).ready(function() {
 
     $("#item-search-box").on('input', function() {
         var search = $("#item-search-box").val().toLowerCase();
-        var all_item = $(".item", $("#all-items"));
+        var all_items = $(".item", $("#all-items"));
         if (search != '') {
-            all_item.hide();
-            all_item.filter(function() {
+            all_items.hide();
+            all_items.filter(function() {
                 var alt = $(this)[0].alt.toLowerCase();
                 return alt.indexOf(search) > -1;
             }).show();
         } else {
-            all_item.show();
+            all_items.show();
         }
     });
 });
