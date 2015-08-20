7a3f94ac48f40321389334bd70d3561bab01faa9
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-19 23:21:10 -0700

    started working on item set information tab

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'httparty'
 require File.expand_path('../lib/item-set-builder.rb', __FILE__)
 
 itemAPI = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?locale=en_US&itemListData=all&api_key=' + ENV["LOL_KEY"]
+champAPI = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=' + ENV["LOL_KEY"]
 
 get '/' do
   File.read(File.join('public', 'index.html'))
@@ -20,6 +21,11 @@ get '/getItems' do
   response = HTTParty.get(itemAPI)
   itemParser = ItemParser.new(response.parsed_response)
   itemParser.getItems.to_json
+end
+
+get '/getChamps' do
+  response = HTTParty.get(champAPI)
+  response.parsed_response['data'].to_json
 end
 
 post '/uploadJSON' do
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -38,6 +38,15 @@ $(document).ready(function() {
         }
     });
 
+    $.get("/getChamps", function(data) {
+        dataJSON = JSON.parse(data);
+        sortedKeys = Object.keys(dataJSON).sort()
+        sortedKeys.forEach(function(champName) {
+            $(".champ-container").append('<div class="col s1 no-padding"><img class="champ-select" id="' + dataJSON[champName]["key"] + '" src="images/champs/' + dataJSON[champName]["key"] + '.png" alt="' + dataJSON[champName]["name"] + '"></div>');
+            new Opentip("#" + dataJSON[champName]["key"], dataJSON[champName]["name"])               
+        });
+    });
+
     $("#set-form-name").on('input', function() {
         $("#download-button").attr('download', $("#set-form-name").val() + ".json")
     });
