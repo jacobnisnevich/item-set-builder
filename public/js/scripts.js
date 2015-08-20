11d46b3b1e1c8cfe6dfb38b40b8b0e94b470cd21
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-20 16:07:35 -0700

    made filter draggable

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,6 +18,8 @@ $(document).ready(function() {
 
     $(".button-collapse").sideNav();
 
+    $(".filter-menu").draggable({ handle: ".filter-form-title" });
+
     $("#item-set-add-block-button").click(function() {
         $("#item-set-blocks").append('<li><div class="collapsible-header grey-text text-darken-2" contentEditable=true>New Item Block</div><div class="collapsible-body grey lighten-3 grey-text text-darken-2"><div class="item-slots clearfix"><div class="item-slot slot-1" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-1">1</div></div><div class="item-slot slot-2" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-2">1</div></div><div class="item-slot slot-3" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-3">1</div></div><div class="item-slot slot-4" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-4">1</div></div><div class="item-slot slot-5" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-5">1</div></div><div class="item-slot slot-6" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-6">1</div></div><div class="item-slot slot-7" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-7">1</div></div><div class="item-slot slot-8" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-8">1</div></div><div class="item-slot slot-9" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-9">1</div></div><div class="item-slot slot-10" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-10">1</div></div></div></div></li>');
         $(".collapsible").collapsible({
@@ -225,9 +227,9 @@ function drop(ev) {
 
 function toggleFilterMenu() {
     if ($('.filter-menu').is(':visible')) {
-        $('.filter-menu').hide('slide', 300);
+        $('.filter-menu').hide();
     } else {
-        $('.filter-menu').show('slide', 300);
+        $('.filter-menu').show();
     }
 }
 
