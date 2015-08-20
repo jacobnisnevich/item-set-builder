feb90d319c2b227bb6d8d4f7af618cfc961a2bac
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-19 19:12:03 -0700

    fixed most merge issues

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
     };
     Opentip.defaultStyle = "leagueItems";
 
-    if (localStorage.getItem('itemSetBuilderData') != null) {
-        loadSessionData();
-    }
+    // if (localStorage.getItem('itemSetBuilderData') != null) {
+    //     loadSessionData();
+    // }
 
     $("#item-set-add-block-button").click(function() {
         $("#item-set-blocks").append('<li><div class="collapsible-header grey-text text-darken-2" contentEditable=true>New Item Block</div><div class="collapsible-body grey lighten-3 grey-text text-darken-2"><div class="item-slots clearfix"><div class="item-slot slot-1" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-1">1</div></div><div class="item-slot slot-2" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-2">1</div></div><div class="item-slot slot-3" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-3">1</div></div><div class="item-slot slot-4" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-4">1</div></div><div class="item-slot slot-5" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-5">1</div></div><div class="item-slot slot-6" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-6">1</div></div><div class="item-slot slot-7" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-7">1</div></div><div class="item-slot slot-8" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-8">1</div></div><div class="item-slot slot-9" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-9">1</div></div><div class="item-slot slot-10" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-10">1</div></div></div></div></li>');
@@ -234,14 +234,14 @@ function createItemBlock(name, itemsArray, itemCountsArray) {
     var itemBlockString = '<li class="active"><div class="collapsible-header grey-text text-darken-2" contentEditable=true>' + name + '</div>';
     itemBlockString = itemBlockString.concat('<div class="collapsible-body grey lighten-3 grey-text text-darken-2"><div class="item-slots clearfix">');
 
-    itemsArray.forEach(function(itemId) {
+    itemsArray.forEach(function(itemId, index) {
         itemsCount++;
-        itemBlockString = itemBlockString.concat('<div class="item-slot" ondrop="drop(event)" ondragover="allowDrop(event)"><img draggable="true" id="' + itemId + '" ondragstart="drag(event)" src="/images/items/' + itemId + '.png"></div>');
-    })
+        itemBlockString = itemBlockString.concat('<div class="item-slot slot-' + itemsCount + '" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-' + itemsCount + '">' + itemCountsArray[index] + '</div><img draggable="true" id="' + itemId + '" ondragstart="drag(event)" src="/images/items/' + itemId + '.png"></div>');
+    });
 
     while (itemsCount < 10) {
-        itemsCount++;
-        itemBlockString = itemBlockString.concat('<div class="item-slot" ondrop="drop(event)" ondragover="allowDrop(event)"></div>');
+        itemsCount++;    
+        itemBlockString = itemBlockString.concat('<div class="item-slot slot-' + itemsCount + '" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-' + itemsCount + '">1</div></div>');
     }
 
     itemBlockString = itemBlockString.concat('</div></div></li>');
@@ -249,6 +249,10 @@ function createItemBlock(name, itemsArray, itemCountsArray) {
     $(".collapsible").collapsible({
         accordion: false
     });
+
+    $(".item-count").filter(function () {
+       return Number($(this).text()) > 1;
+    }).show();
 }
 
 // JSON data manipulation functions
@@ -309,18 +313,13 @@ function createJSONObject() {
         $.each($($("#item-set-blocks li")[itemBlock]).find("img"), function(item) {
             block.items.push({
                 "id": $($($("#item-set-blocks li")[itemBlock]).find("img")[item]).attr("id"),
-                "count": "1"
+                "count": $($("#item-set-blocks li")[itemBlock]).find('.count-' + $($($("#item-set-blocks li")[itemBlock]).find("img")[item]).parent().attr('class').match(/slot-(\d+)/)[1]).text()
             });
         });
         obj.blocks.push(block);
     });
 
-    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
-    
-    $("#download-button").attr('href', 'data:' + data);
-    if ($("#set-form-name").val() == "") {
-        $("#download-button").attr('download', "Unnamed_Item_Set.json");
-    }
+    return obj;
 }
 
 //scoots items from index_end to index_start
