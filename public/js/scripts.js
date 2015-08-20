6eea3da9cc684a27554dc9bccf6e0c7e51029750
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-20 03:08:05 -0700

    created toggle all item blocks button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var toggleOn = true;
+
     Opentip.styles.leagueItems = {
         extends: "alert",
         stem: true,
@@ -15,6 +17,8 @@ $(document).ready(function() {
     if (localStorage.getItem('itemSetBuilderData') != null) {
         loadSessionData();
     }
+
+    toggleItemBlocks(true);
 
     $("#item-set-add-block-button").click(function() {
         $("#item-set-blocks").append('<li><div class="collapsible-header grey-text text-darken-2" contentEditable=true>New Item Block</div><div class="collapsible-body grey lighten-3 grey-text text-darken-2"><div class="item-slots clearfix"><div class="item-slot slot-1" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-1">1</div></div><div class="item-slot slot-2" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-2">1</div></div><div class="item-slot slot-3" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-3">1</div></div><div class="item-slot slot-4" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-4">1</div></div><div class="item-slot slot-5" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-5">1</div></div><div class="item-slot slot-6" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-6">1</div></div><div class="item-slot slot-7" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-7">1</div></div><div class="item-slot slot-8" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-8">1</div></div><div class="item-slot slot-9" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-9">1</div></div><div class="item-slot slot-10" ondrop="drop(event)" ondragover="allowDrop(event)"><div class="item-count count-10">1</div></div></div></div></li>');
@@ -65,6 +69,11 @@ $(document).ready(function() {
 
     $("#save-button").click(function() {
         saveSessionData();
+    });
+
+    $('#toggle-button').click(function() {
+        toggleItemBlocks(toggleOn);
+        toggleOn = !toggleOn;
     });
 
     $("#item-search-box").on('input', function() {
@@ -228,6 +237,14 @@ function clearSessionData() {
 
 // Item block manipulation functions
 
+function toggleItemBlocks(open) {
+    if (!open) {
+        $('.collapsible-header').parent('li.active').find('.collapsible-header').click();
+    } else {
+        $('.collapsible-header').parent().not('li.active').find('.collapsible-header').click();
+    }
+}
+
 function resetItemBlocks() {
     removeItemBlocks();
     createItemBlock('New Item Block', [], []);
@@ -240,7 +257,7 @@ function removeItemBlocks() {
 function createItemBlock(name, itemsArray, itemCountsArray) {
     var itemsCount = 0;
 
-    var itemBlockString = '<li class="active"><div class="collapsible-header grey-text text-darken-2" contentEditable=true>' + name + '</div>';
+    var itemBlockString = '<li><div class="collapsible-header grey-text text-darken-2" contentEditable=true>' + name + '</div>';
     itemBlockString = itemBlockString.concat('<div class="collapsible-body grey lighten-3 grey-text text-darken-2"><div class="item-slots clearfix">');
 
     itemsArray.forEach(function(itemId, index) {
