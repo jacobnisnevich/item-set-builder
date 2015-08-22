567c5b01401ab6a1cabc00f56528ecef88ce3a36
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-21 22:10:29 -0700

    updated branch with data files

---
+++ lib/item-set-builder/remove_item_duplicates.rb
@@ -0,0 +1,12 @@
+require 'json'
+
+file = File.read('../item-efficiency-raw.json')
+hash = JSON.parse(file)
+
+no_dupes_hash = hash.uniq do |item| 
+  item["Item"]
+end
+
+File.open('../item-efficiency-nodupes.json', 'w') do |file|
+  file.write(no_dupes_hash.to_json)
+end
