dc701795900c23eb86817cb166e901d7ac72c030
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-23 21:33:38 -0700

    created combined item efficiency file

---
+++ lib/item-set-builder/combine_item_cases.rb
@@ -0,0 +1,46 @@
+require 'json'
+
+file = File.read('../id-to-name.json')
+id_to_name = JSON.parse(file)
+
+file = File.read('../item-efficiency-nodupes.json')
+item_efficiency = JSON.parse(file)
+
+item_efficiency_combined = []
+
+id_to_name.each do |id, name|
+  item = {}
+  item[id] = name
+
+  matching_items = item_efficiency.select {|item| item["Item"].to_s().start_with?(name)}
+
+  if matching_items.empty?
+    item["base"] = true
+  else
+    item["base"] = false
+    item["cost"] = matching_items[0]["Cost"]
+    item["cases"] = []
+    matching_items.each do |matching_item_case| 
+      item_case = {}
+
+      if matching_item_case["Item"].scan(/\(([^\)]+)\)/).empty?
+        item_case_name = "normal"
+      else
+        item_case_name = matching_item_case["Item"].scan(/\(([^\)]+)\)/)[0][0]
+      end
+
+      item_case[item_case_name] = {}
+      item_case[item_case_name]["Efficiency"] = matching_item_case["Efficiency"]
+      item_case[item_case_name]["Gold Value"] = matching_item_case["Gold Value"]
+      item_case[item_case_name]["Gold Efficiency Ratio"] = matching_item_case["Gold Efficiency Ratio"]
+
+      item["cases"].push(item_case)
+    end
+  end
+
+  item_efficiency_combined.push(item)
+end
+
+File.open('../item-efficiency-combined.json', 'w') do |file|
+  file.write(item_efficiency_combined.to_json)
+end
