02a72130c4b8f5963aab75f1804d05b61d004c40
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2015-08-25 23:32:05 -0700

    made ruby script for compile javascript

--- buildClient.ps1
+++ buildClient.ps1
@@ -1,7 +1,7 @@
 Split-Path $MyInvocation.MyCommand.Path | Set-Location
 
 $javaPath           = "C:\Program Files (x86)\Java\jre1.8.0_45\bin\java.exe" 
-$closurePath        = "c:\tools\closure-compiler\compiler.jar"
+$closurePath        = "C:\Tools\closure-compiler\compiler.jar"
 
 $version = "0.1.1"
 
--- public/js/bildr.js
+++ public/js/bildr.js
@@ -1,3 +1,4 @@
+
 var global = {
 	setName: 'Unnamed_Item_Set',
 	selectedMap: '',
