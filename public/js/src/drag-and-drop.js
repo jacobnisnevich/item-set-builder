adcfe302e65e948825a5f16e23d8488fb9b2b825
Jason Yang <jasonyang96@ucla.edu>
2015-08-27 20:39:58 -0500

    dem comments

--- public/js/src/drag-and-drop.js
+++ public/js/src/drag-and-drop.js
@@ -22,9 +22,9 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    //if dropping into trash
+    // if dropping into trash
     if (ev.target.id == "trash") {
-        if (global.source_item_slots.filter(".item-slot").length == 0) {//from all-items
+        if (global.source_item_slots.filter(".item-slot").length == 0) {// from all-items
             return;
         }
         var index = Number(ev.dataTransfer.getData("index"));
@@ -35,7 +35,7 @@ function drop(ev) {
         return;
     }
 
-    //else dropping into item-set builder
+    // else dropping into item-set builder
     var data = ev.dataTransfer.getData("text");
     var slot_filled = $(ev.target.parentElement).not(".item-slot").length == 0;
     var item_slots;
@@ -62,16 +62,16 @@ function drop(ev) {
         });
     }
 
-    //if dragging from one item-set block to another
+    // if dragging from one item-set block to another
     if (global.source_item_slots.filter(".item-slot").length > 0 && !global.source_item_slots.is(item_slots)) {
         //delete source item if source is another item-slot block
         var index = Number(ev.dataTransfer.getData("index"));
         scootRight(index, global.source_index_empty, global.source_item_slots);
         global.source_item_slots.eq(global.source_item_slots).children().remove("img");
         global.source_item_slots.eq(global.source_item_slots).find(".item-count").html(1);
-        global.source_item_slots.eq(global.source_index_empty).find(".item-count").hide();
+        global.source_item_slots.eq(global.source_item_slots).find(".item-count").hide();
     }
-    //if dragging item from item-set block to same item-set block
+    // if dragging item from item-set block to same item-set block
     else if (ev.dataTransfer.getData("parent").indexOf("item-slot") > -1 && item_slots.is(global.source_item_slots)) {
         var index_source = Number(ev.dataTransfer.getData("index"));
         if (slot_filled) {
@@ -94,22 +94,22 @@ function drop(ev) {
                         } else { //reached stack cap
                             scootRight(index_drop, index_source, item_slots);
                         }
-                    } //else swapping same item so do nothing
+                    } // else swapping same item so do nothing
                 }
-                else { //not same item
+                else { // not same item
                     scootLeft(index_drop, index_source, item_slots);
                 }
             }
-            else { //source < destination
-                //stack if item is stackable
+            else { // source < destination
+                // stack if item is stackable
                 if (ev.target.id == data) {
                     if (data == '2003' || data == '2004' || data == '2043' || data == '2044') { //if stackable
                         var countElement = $(ev.target).parent().find('.item-count');
                         var countNumber = Number($(ev.target).parent().find('.item-count').html());
-                        if ((data == '2003' && countNumber < 5) || //health potion
-                            (data == '2004' && countNumber < 5) || //mana potion
-                            (data == '2043' && countNumber < 2) || //vision ward
-                            (data == '2044' && countNumber < 3))   //stealth ward
+                        if ((data == '2003' && countNumber < 5) || // health potion
+                            (data == '2004' && countNumber < 5) || // mana potion
+                            (data == '2043' && countNumber < 2) || // vision ward
+                            (data == '2044' && countNumber < 3))   // stealth ward
                         { 
                             $(countElement).html(++countNumber);
                             $(countElement).show();
@@ -117,51 +117,51 @@ function drop(ev) {
                             item_slots.eq(index_empty - 1).remove("img");
                             item_slots.eq(index_empty - 1).find(".item-count").html(1);
                             item_slots.eq(index_empty - 1).find(".item-count").hide();
-                        } else { //reached stack cap
+                        } else { // reached stack cap
                             scootLeft(index_source, index_drop, item_slots);
                         }
-                    } //else swapping same item so do nothing
+                    } // else swapping same item so do nothing
                 }
-                else { //not same item
+                else { // not same item
                     scootRight(index_source, index_drop, item_slots);
                 }
             }
         }
-        else { //empty, scoot
+        else { // empty, scoot
             scootRight(index_source, index_empty - 1, item_slots);
         }
     }
-    else { //came from all-items box or from another item-slot block
-        //if slot has item
+    else { // came from all-items box or from another item-slot block
+        // if slot has item
         if (slot_filled) {
             //stack if item is stackable
             if (ev.target.id == data) {
                 var countElement = $(ev.target).parent().find('.item-count');
                 var countNumber = Number($(ev.target).parent().find('.item-count').html());
-                if ((data == '2003' && countNumber < 5) || //health potion
-                    (data == '2004' && countNumber < 5) || //mana potion
-                    (data == '2043' && countNumber < 2) || //vision ward
-                    (data == '2044' && countNumber < 3))   //stealth ward
+                if ((data == '2003' && countNumber < 5) || // health potion
+                    (data == '2004' && countNumber < 5) || // mana potion
+                    (data == '2043' && countNumber < 2) || // vision ward
+                    (data == '2044' && countNumber < 3))   // stealth ward
                 { 
                     $(countElement).html(++countNumber);
                     $(countElement).show();
-                } else if (!isFull()) { //not stackable item or reached stack cap
+                } else if (!isFull()) { // not stackable item or reached stack cap
                     item_slots.eq(index_empty).append(document.getElementById(data).cloneNode(true));
                     scootLeft(index_drop, index_empty, item_slots);
                 }
             }
-            else if (!isFull()) { //not same item
+            else if (!isFull()) { // not same item
                 item_slots.eq(index_empty).append(document.getElementById(data).cloneNode(true));
                 scootLeft(index_drop, index_empty, item_slots);
             }
         }
-        else { //empty, append to end
+        else { // empty, append to end
             item_slots.eq(index_empty).append(document.getElementById(data).cloneNode(true));
         }
     }
 }
 
-//scoots items from index_end to index_start
+// scoots items from index_end to index_start
 function scootLeft(index_start, index_end, item_slots) {
     for (var i = index_end - 1; i >= index_start; i--) {
         var leftCountElement = $(item_slots.eq(i).find('.item-count'));
@@ -169,18 +169,18 @@ function scootLeft(index_start, index_end, item_slots) {
         var rightCountElement = $(item_slots.eq(i + 1).find('.item-count'))
         var rightCountNumber = Number(item_slots.eq(i + 1).find('.item-count').html());
 
-        //swap count numbers
+        // swap count numbers
         var temp = leftCountNumber;
         $(leftCountElement).html(rightCountNumber);
         $(rightCountElement).html(temp);
 
-        //swap hidden-ness
+        // swap hidden-ness
         var left_hidden = $(leftCountElement).is(":hidden");
         var right_hidden = $(rightCountElement).is(":hidden")
         left_hidden ? rightCountElement.hide() : rightCountElement.show();
         right_hidden ? leftCountElement.hide() : leftCountElement.show();
 
-        //swap item images
+        // swap item images
         var left_item = item_slots.eq(i).find(".item").detach();
         var right_item = item_slots.eq(i + 1).find(".item").detach();
         item_slots.eq(i).append(right_item);
@@ -188,7 +188,7 @@ function scootLeft(index_start, index_end, item_slots) {
     };
 }
 
-//scoots items from index_start to index_end
+// scoots items from index_start to index_end
 function scootRight(index_start, index_end, item_slots) {
     for (var i = index_start; i < index_end; i++) {
         var leftCountElement = $(item_slots.eq(i + 1).find('.item-count'));
@@ -196,18 +196,18 @@ function scootRight(index_start, index_end, item_slots) {
         var rightCountElement = $(item_slots.eq(i).find('.item-count'))
         var rightCountNumber = Number(item_slots.eq(i).find('.item-count').html());
 
-        //swap count numbers
+        // swap count numbers
         var temp = leftCountNumber;
         $(leftCountElement).html(rightCountNumber);
         $(rightCountElement).html(temp);
 
-        //swap hidden-ness
+        // swap hidden-ness
         var source_hidden = $(leftCountElement).is(":hidden");
         var destination_hidden = $(rightCountElement).is(":hidden")
         source_hidden ? rightCountElement.hide() : rightCountElement.show();
         destination_hidden ? leftCountElement.hide() : leftCountElement.show();
 
-        //swap item images
+        // swap item images
         var leftItem = item_slots.eq(i).find(".item").detach();
         var rightItem = item_slots.eq(i + 1).find(".item").detach();
         item_slots.eq(i).append(rightItem);
@@ -215,7 +215,7 @@ function scootRight(index_start, index_end, item_slots) {
     };
 }
 
-//returns true if item-slots are full
+// returns true if item-slots are full
 function isFull() {
     var returnval = true;
     $(".item-slots").children().each(function() {
